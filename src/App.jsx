90e1c4c3b22b083e53f8c67a2e704689cab5e0b7
Nathan-Front <jonathantindungan3@gmail.com>
2026-06-27 20:58:52 +0900

    added page loading effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,53 @@
 import { HashRouter, Routes } from "react-router-dom";
 import { Route } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import Nav from "./components/navigation/nav.jsx";
 import Foot from "./components/footer/foot.jsx";
 import Voyage from "./components/voyage/voyage.jsx";
 import Manifest from "./components/manifest/manifest.jsx";
 import DropAnchor from "./components/contact/dropAnchor.jsx";
+
+function Loading() {
+  return (
+    <div className="loading">
+      <div className="pageSpinner"></div>
+      <p>Loading your voyage...</p>
+    </div>
+  );
+}
+
 function AppContent() {
   const location = useLocation();
-
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     let page = location.pathname.replace("/", "");
-
     if (page === "") {
-      page = "voyage"; // default first page
+      page = "voyage"; //default first page
     }
-
     document.body.dataset.page = page;
-
+    setLoading(true);
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 700);
     return () => {
       delete document.body.dataset.page;
+      clearTimeout(timer);
     };
   }, [location]);
+
   return (
     <>
       <Nav />
-      <Routes>
-        <Route path="/" element={<Voyage />} />
-        <Route path="/manifest" element={<Manifest />} />
-        <Route path="/dropAnchor" element={<DropAnchor />} />
-      </Routes>
+      {loading ? (
+        <Loading />
+      ) : (
+        <Routes>
+          <Route path="/" element={<Voyage />} />
+          <Route path="/manifest" element={<Manifest />} />
+          <Route path="/dropAnchor" element={<DropAnchor />} />
+        </Routes>
+      )}
       <Foot />
     </>
   );

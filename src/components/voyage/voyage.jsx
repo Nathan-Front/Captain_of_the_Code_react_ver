689fd15548c028b53c184636abd09fe73bff5306
Nathan-Front <jonathantindungan3@gmail.com>
2026-07-02 20:46:13 +0900

    removed lazy loading for voyagefirstSection component

--- src/components/voyage/voyage.jsx
+++ src/components/voyage/voyage.jsx
@@ -1,14 +1,16 @@
 import { lazy, Suspense } from "react";
-const VoyageFirstSection = lazy(() => import("./voyageFirstSection.jsx"));
+import VoyageFirstSection from "./voyageFirstSection.jsx";
 const VoyageSecondSection = lazy(() => import("./voyageSecondSection.jsx"));
 const BackToTop = lazy(() => import("./backToTop.jsx"));
 function Voyage() {
   return (
-    <Suspense>
+    <>
       <VoyageFirstSection />
-      <VoyageSecondSection />
-      <BackToTop />
-    </Suspense>
+      <Suspense>
+        <VoyageSecondSection />
+        <BackToTop />
+      </Suspense>
+    </>
   );
 }
 
